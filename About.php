607fb0374fe0b26cb78e9854f462203f907efdf0
richmond janus rafii aryanto <53498537+Richmondjanusrafiiaryanto@users.noreply.github.com>
2020-04-23 22:41:22 +0700

    Add files via upload

---
+++ About.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>RJR</title>
+    <link rel="stylesheet" href="Style.css">
+  </head>
+  <body>
+<div id="canvas">
+
+    <div align="center" id="header">
+      <img src="BahanWEB.png" alt="">
+      <li>Layanan Kesehatan Check COVID-19</li>
+    </div>
+
+    <div id="menu">
+      <ul>
+        <li class="utama"><a href="http://localhost/TugasAkhir/index.php">Beranda</a></li>
+        <li class="utama"><a href="http://localhost/TugasAkhir/About.php">About</a></li>
+        <li class="utama"><a href="">Akun</a>
+            <ul>
+            <li><a href="http://localhost/TugasAkhir/Login.php">Logout</a></li>
+            </ul>
+            </li>
+      </ul>
+    </div>
+
+    <div align="center" id="isi">
+      <ul>
+        <li>PEMBUAT</li>
+        <li><img height="150" width="150" src="malang.png" alt="">
+          <li>Nama : Richmond Janus</li>
+          <li>Umur : 16</li>
+          <li>Tempat/Tanggal Lahir : Sragen,04-01-2004</li>
+        </li>
+      </ul>
+    <div id="footer">
+      Copyright 2020 - RJR_MV.
+    </div>
+</div>
+  </body>
+</html>
